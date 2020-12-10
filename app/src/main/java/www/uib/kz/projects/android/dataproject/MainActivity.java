43c2d78dc62a1d2071013cd854fc807dd66bc1eb
beimbet87 <beimbet.daribaev@gmail.com>
2020-12-10 10:37:07 +0600

    First Project Commit!

---
+++ app/src/main/java/www/uib/kz/projects/android/dataproject/MainActivity.java
@@ -0,0 +1,14 @@
+package www.uib.kz.projects.android.dataproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
