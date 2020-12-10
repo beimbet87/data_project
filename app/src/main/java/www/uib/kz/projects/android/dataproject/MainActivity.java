60977fdc5e8d3be96b465ea986474d8286099440
beimbet87 <beimbet.daribaev@gmail.com>
2020-12-10 10:42:09 +0600

    Add send button realization!

--- app/src/main/java/www/uib/kz/projects/android/dataproject/MainActivity.java
+++ app/src/main/java/www/uib/kz/projects/android/dataproject/MainActivity.java
@@ -3,12 +3,28 @@ package www.uib.kz.projects.android.dataproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.snackbar.Snackbar;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView btnSend;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSend = findViewById(R.id.btn_send);
+
+        btnSend.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btn_send) {
+            Snackbar.make(view, "Message", Snackbar.LENGTH_LONG).show();
+        }
     }
 }
